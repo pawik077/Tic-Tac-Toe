50c64719f60916b3142837791bf5368f3e5a5d38
pawik077 <pawik.r.s.077@gmail.com>
2020-05-25 18:07:41 +0200

    Replaced win, lose, and draw windows with single gameOver() function

--- tictactoe/Display.cpp
+++ tictactoe/Display.cpp
@@ -14,9 +14,9 @@ int field(sf::Event event, int dimension, const std::unique_ptr<std::unique_ptr<
 	return -1;
 }
 
-bool win(int playerScore, int botScore) {
+bool gameOver(int playerScore, int botScore, char state) {
 	std::string scoreString = "Current score: Player " + std::to_string(playerScore) + ":" + std::to_string(botScore) + " Bot";
-	sf::RenderWindow window(sf::VideoMode(580, 224), "YOU WIN", sf::Style::Titlebar);
+	sf::RenderWindow window(sf::VideoMode(580, 224), "", sf::Style::Titlebar);
 	sf::Font font;
 	font.loadFromFile("arial.ttf");
 	sf::Text announ;
@@ -30,7 +30,6 @@ bool win(int playerScore, int botScore) {
 	button1.setPosition(sf::Vector2f(80, 154));
 	button2.setPosition(sf::Vector2f(360, 154));
 	announ.setFont(font);
-	announ.setString("YOU WIN!");
 	announ.setCharacterSize(100);
 	announ.setFillColor(sf::Color::Black);
 	b1text.setFont(font);
@@ -45,194 +44,32 @@ bool win(int playerScore, int botScore) {
 	score.setString(scoreString);
 	score.setCharacterSize(20);
 	score.setFillColor(sf::Color::Black);
-	announ.setOrigin(announ.getLocalBounds().width / 2, announ.getLocalBounds().height / 2);
-	announ.setPosition(sf::Vector2f(290, 30));
 	b1text.setOrigin(b1text.getLocalBounds().width / 2, b1text.getLocalBounds().height / 2);
 	b1text.setPosition(sf::Vector2f(148, 171));
 	b2text.setOrigin(b2text.getLocalBounds().width / 2, b2text.getLocalBounds().height / 2);
 	b2text.setPosition(sf::Vector2f(429, 171));
 	score.setOrigin(score.getLocalBounds().width / 2, score.getLocalBounds().height / 2);
 	score.setPosition(290, 119);
-	while(window.isOpen()) {
-		sf::Event event;
-		while(window.pollEvent(event)) {
-			switch(event.type) {
-				case sf::Event::MouseButtonPressed:
-					if(event.mouseButton.button == sf::Mouse::Left) {
-						if(event.mouseButton.x >= button1.getPosition().x &&
-							 event.mouseButton.x <= button1.getPosition().x + button1.getSize().x &&
-							 event.mouseButton.y >= button1.getPosition().y &&
-							 event.mouseButton.y <= button1.getPosition().y + button1.getSize().y) {
-							window.close();
-							return false;
-						} else if(event.mouseButton.x >= button2.getPosition().x &&
-											event.mouseButton.x <= button2.getPosition().x + button2.getSize().x &&
-											event.mouseButton.y >= button2.getPosition().y &&
-											event.mouseButton.y <= button2.getPosition().y + button2.getSize().y) {
-							window.close();
-							return true;
-						}
-					}
-					break;
-				case sf::Event::MouseMoved:
-					if(event.mouseMove.x >= button1.getPosition().x &&
-						 event.mouseMove.x <= button1.getPosition().x + button1.getSize().x &&
-						 event.mouseMove.y >= button1.getPosition().y &&
-						 event.mouseMove.y <= button1.getPosition().y + button1.getSize().y)
-						button1.setFillColor(sf::Color(150, 150, 150));
-					else if(event.mouseMove.x >= button2.getPosition().x &&
-									event.mouseMove.x <= button2.getPosition().x + button2.getSize().x &&
-									event.mouseMove.y >= button2.getPosition().y &&
-									event.mouseMove.y <= button2.getPosition().y + button2.getSize().y)
-						button2.setFillColor(sf::Color(150, 150, 150));
-					else {
-						button1.setFillColor(sf::Color(200, 200, 200));
-						button2.setFillColor(sf::Color(200, 200, 200));
-					}
-					break;
-			}
-		}
-		window.clear(sf::Color::White);
-		window.draw(announ);
-		window.draw(button1);
-		window.draw(button2);
-		window.draw(b1text);
-		window.draw(b2text);
-		window.draw(score);
-		window.display();
+	switch(state) {
+		case 'w':
+			window.setTitle("YOU WIN");
+			announ.setString("YOU WIN!");
+			announ.setOrigin(announ.getLocalBounds().width / 2, announ.getLocalBounds().height / 2);
+			announ.setPosition(sf::Vector2f(290, 30));
+			break;
+		case 'l':
+			window.setTitle("YOU LOSE");
+			announ.setString("YOU LOSE!");
+			announ.setOrigin(announ.getLocalBounds().width / 2, announ.getLocalBounds().height / 2);
+			announ.setPosition(sf::Vector2f(290, 30));
+			break;
+		case 'd':
+			window.setTitle("DRAW");
+			announ.setString("DRAW!");
+			announ.setOrigin(announ.getLocalBounds().width / 2, announ.getLocalBounds().height / 2);
+			announ.setPosition(sf::Vector2f(283, 30));
+			break;
 	}
-	return false;
-}
-
-bool lose(int playerScore, int botScore) {
-	std::string scoreString = "Current score: Player " + std::to_string(playerScore) + ":" + std::to_string(botScore) + " Bot";
-	sf::RenderWindow window(sf::VideoMode(580, 224), "YOU LOSE", sf::Style::Titlebar);
-	sf::Font font;
-	font.loadFromFile("arial.ttf");
-	sf::Text announ;
-	sf::Text b1text;
-	sf::Text b2text;
-	sf::Text score;
-	sf::RectangleShape button1(sf::Vector2f(140, 50));
-	sf::RectangleShape button2(sf::Vector2f(140, 50));
-	button1.setFillColor(sf::Color(200, 200, 200));
-	button2.setFillColor(sf::Color(200, 200, 200));
-	button1.setPosition(sf::Vector2f(80, 154));
-	button2.setPosition(sf::Vector2f(360, 154));
-	announ.setFont(font);
-	announ.setString("YOU LOSE!");
-	announ.setCharacterSize(100);
-	announ.setFillColor(sf::Color::Black);
-	b1text.setFont(font);
-	b1text.setString("Finish");
-	b1text.setCharacterSize(30);
-	b1text.setFillColor(sf::Color::Black);
-	b2text.setFont(font);
-	b2text.setString("Continue");
-	b2text.setCharacterSize(30);
-	b2text.setFillColor(sf::Color::Black);
-	score.setFont(font);
-	score.setString(scoreString);
-	score.setCharacterSize(20);
-	score.setFillColor(sf::Color::Black);
-	announ.setOrigin(announ.getLocalBounds().width / 2, announ.getLocalBounds().height / 2);
-	announ.setPosition(sf::Vector2f(290, 30));
-	b1text.setOrigin(b1text.getLocalBounds().width / 2, b1text.getLocalBounds().height / 2);
-	b1text.setPosition(sf::Vector2f(148, 171));
-	b2text.setOrigin(b2text.getLocalBounds().width / 2, b2text.getLocalBounds().height / 2);
-	b2text.setPosition(sf::Vector2f(429, 171));
-	score.setOrigin(score.getLocalBounds().width / 2, score.getLocalBounds().height / 2);
-	score.setPosition(290, 119);
-	while(window.isOpen()) {
-		sf::Event event;
-		while(window.pollEvent(event)) {
-			switch(event.type) {
-				case sf::Event::MouseButtonPressed:
-					if(event.mouseButton.button == sf::Mouse::Left) {
-						if(event.mouseButton.x >= button1.getPosition().x &&
-							 event.mouseButton.x <= button1.getPosition().x + button1.getSize().x &&
-							 event.mouseButton.y >= button1.getPosition().y &&
-							 event.mouseButton.y <= button1.getPosition().y + button1.getSize().y) {
-							window.close();
-							return false;
-						} else if(event.mouseButton.x >= button2.getPosition().x &&
-											event.mouseButton.x <= button2.getPosition().x + button2.getSize().x &&
-											event.mouseButton.y >= button2.getPosition().y &&
-											event.mouseButton.y <= button2.getPosition().y + button2.getSize().y) {
-							window.close();
-							return true;
-						}
-					}
-					break;
-				case sf::Event::MouseMoved:
-					if(event.mouseMove.x >= button1.getPosition().x &&
-						 event.mouseMove.x <= button1.getPosition().x + button1.getSize().x &&
-						 event.mouseMove.y >= button1.getPosition().y &&
-						 event.mouseMove.y <= button1.getPosition().y + button1.getSize().y)
-						button1.setFillColor(sf::Color(150, 150, 150));
-					else if(event.mouseMove.x >= button2.getPosition().x &&
-									event.mouseMove.x <= button2.getPosition().x + button2.getSize().x &&
-									event.mouseMove.y >= button2.getPosition().y &&
-									event.mouseMove.y <= button2.getPosition().y + button2.getSize().y)
-						button2.setFillColor(sf::Color(150, 150, 150));
-					else {
-						button1.setFillColor(sf::Color(200, 200, 200));
-						button2.setFillColor(sf::Color(200, 200, 200));
-					}
-					break;
-			}
-		}
-		window.clear(sf::Color::White);
-		window.draw(announ);
-		window.draw(button1);
-		window.draw(button2);
-		window.draw(b1text);
-		window.draw(b2text);
-		window.draw(score);
-		window.display();
-	}
-	return false;
-}
-
-bool draw(int playerScore, int botScore) {
-	std::string scoreString = "Current score: Player " + std::to_string(playerScore) + ":" + std::to_string(botScore) + " Bot";
-	sf::RenderWindow window(sf::VideoMode(580, 224), "DRAW", sf::Style::Titlebar);
-	sf::Font font;
-	font.loadFromFile("arial.ttf");
-	sf::Text announ;
-	sf::Text b1text;
-	sf::Text b2text;
-	sf::Text score;
-	sf::RectangleShape button1(sf::Vector2f(140, 50));
-	sf::RectangleShape button2(sf::Vector2f(140, 50));
-	button1.setFillColor(sf::Color(200, 200, 200));
-	button2.setFillColor(sf::Color(200, 200, 200));
-	button1.setPosition(sf::Vector2f(80, 154));
-	button2.setPosition(sf::Vector2f(360, 154));
-	announ.setFont(font);
-	announ.setString("DRAW!");
-	announ.setCharacterSize(100);
-	announ.setFillColor(sf::Color::Black);
-	b1text.setFont(font);
-	b1text.setString("Finish");
-	b1text.setCharacterSize(30);
-	b1text.setFillColor(sf::Color::Black);
-	b2text.setFont(font);
-	b2text.setString("Continue");
-	b2text.setCharacterSize(30);
-	b2text.setFillColor(sf::Color::Black);
-	score.setFont(font);
-	score.setString(scoreString);
-	score.setCharacterSize(20);
-	score.setFillColor(sf::Color::Black);
-	announ.setOrigin(announ.getLocalBounds().width / 2, announ.getLocalBounds().height / 2);
-	announ.setPosition(sf::Vector2f(283, 30));
-	b1text.setOrigin(b1text.getLocalBounds().width / 2, b1text.getLocalBounds().height / 2);
-	b1text.setPosition(sf::Vector2f(148, 171));
-	b2text.setOrigin(b2text.getLocalBounds().width / 2, b2text.getLocalBounds().height / 2);
-	b2text.setPosition(sf::Vector2f(429, 171));
-	score.setOrigin(score.getLocalBounds().width / 2, score.getLocalBounds().height / 2);
-	score.setPosition(290, 119);
 	while(window.isOpen()) {
 		sf::Event event;
 		while(window.pollEvent(event)) {
--- tictactoe/Display.hh
+++ tictactoe/Display.hh
@@ -2,6 +2,4 @@
 #include <SFML/Graphics.hpp>
 
 int field(sf::Event event, int dimension, const std::unique_ptr<std::unique_ptr<sf::RectangleShape[]>[]>& sq);
-bool win(int playerScore, int botScore);
-bool lose(int playerScore, int botScore);
-bool draw(int playerScore, int botScore);
+bool gameOver(int playerScore, int botScore, char state);
