5657981f6419284830b6fb63352e7f8374463013
pawik077 <pawik.r.s.077@gmail.com>
2020-05-25 16:26:48 +0200

    Moved display-handling functions to Display.cpp

---
+++ tictactoe/Display.hh
@@ -0,0 +1,6 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+int field(sf::Event event, int dimension, const std::unique_ptr<std::unique_ptr<sf::RectangleShape[]>[]>& sq);
+bool win(int playerScore, int botScore);
+bool lose(int playerScore, int botScore);
