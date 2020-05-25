edcba77d7780928c95955512afeacd55a987f2be
pawik077 <pawik.r.s.077@gmail.com>
2020-05-25 17:34:08 +0200

    Added draw window

--- tictactoe/Display.hh
+++ tictactoe/Display.hh
@@ -4,3 +4,4 @@
 int field(sf::Event event, int dimension, const std::unique_ptr<std::unique_ptr<sf::RectangleShape[]>[]>& sq);
 bool win(int playerScore, int botScore);
 bool lose(int playerScore, int botScore);
+bool draw(int playerScore, int botScore);
