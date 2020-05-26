346d4284409579aa4a06fe0bb625e7f1f904ff65
pawik077 <pawik.r.s.077@gmail.com>
2020-05-26 16:42:14 +0200

    Added basic game mechanics

--- tictactoe/main.cpp
+++ tictactoe/main.cpp
@@ -3,108 +3,146 @@
 #include "Display.hh"
 
 int main() {
-	int dimension = 5;
-	char playerTag = 'x';
-	char botTag = playerTag == 'x' ? 'o' : 'x';
-	int winCondition = 3;
-	int playerScore = 0;
-	int botScore = 0;
-	Board board(dimension);
-	//std::cout << "Dim: ";
-	//std::cin >> dimension;
-	//std::cout << "Num: ";
-	//std::cin >> numberToWin;
-	sf::Font font;
-	font.loadFromFile("arial.ttf");
-	sf::Text scoreBanner;
-	scoreBanner.setFont(font);
-	scoreBanner.setCharacterSize(25);
-	scoreBanner.setFillColor(sf::Color::Black);
-	std::string scoreText;
-	sf::ContextSettings windowSettings;
-	windowSettings.antialiasingLevel = 16;
-	sf::RenderWindow window(sf::VideoMode(10 + 110 * dimension, 110 * dimension + 40), "Tic-Tac-Toe", sf::Style::Titlebar | sf::Style::Close, windowSettings);
-	sf::RectangleShape upperBar(sf::Vector2f(static_cast<float>(10 + 110 * dimension), 30));
-	upperBar.setFillColor(sf::Color(200, 200, 200));
-	std::unique_ptr<std::unique_ptr<sf::RectangleShape[]>[]> sq = std::make_unique<std::unique_ptr<sf::RectangleShape[]>[]>(dimension);;
-	for(int i = 0; i < dimension; ++i) sq[i] = std::make_unique<sf::RectangleShape[]>(dimension);
-	std::unique_ptr<std::unique_ptr<sf::CircleShape[]>[]> circles = std::make_unique<std::unique_ptr<sf::CircleShape[]>[]>(dimension);
-	for(int i = 0; i < dimension; ++i) circles[i] = std::make_unique<sf::CircleShape[]>(dimension);
-	std::unique_ptr<std::unique_ptr<sf::RectangleShape[][2]>[]> crosses = std::make_unique<std::unique_ptr<sf::RectangleShape[][2]>[]>(dimension);
-	for(int i = 0; i < dimension; ++i) crosses[i] = std::make_unique<sf::RectangleShape[][2]>(dimension);
-	for(int i = 0; i < dimension; ++i) {
-		for(int j = 0; j < dimension; ++j) {
-			sq[i][j].setSize(sf::Vector2f(100, 100));
-			sq[i][j].setFillColor(sf::Color(200, 200, 200));
-			sq[i][j].setPosition(sf::Vector2f(10, 40));
-			sq[i][j].move(sf::Vector2f(static_cast<float>(110 * j), static_cast<float>(110 * i)));
-			circles[i][j].setRadius(40);
-			circles[i][j].setPointCount(20);
-			circles[i][j].setPosition(sf::Vector2f(20, 50));
-			circles[i][j].move(sf::Vector2f(static_cast<float>(110 * j), static_cast<float>(110 * i)));
-			circles[i][j].setFillColor(sf::Color(200, 200, 200));
-			circles[i][j].setOutlineThickness(-5);
-			circles[i][j].setOutlineColor(sf::Color::Black);
-			crosses[i][j][0].setSize(sf::Vector2f(113, 5));
-			crosses[i][j][0].setFillColor(sf::Color::Black);
-			crosses[i][j][0].setOrigin(57, 3);
-			crosses[i][j][0].setPosition(sf::Vector2f(60, 91));
-			crosses[i][j][0].move(sf::Vector2f(static_cast<float>(110 * j), static_cast<float>(110 * i)));
-			crosses[i][j][0].rotate(45);
-			crosses[i][j][1].setSize(sf::Vector2f(113, 5));
-			crosses[i][j][1].setFillColor(sf::Color::Black);
-			crosses[i][j][1].setOrigin(57, 3);
-			crosses[i][j][1].setPosition(sf::Vector2f(59, 90));
-			crosses[i][j][1].move(sf::Vector2f(static_cast<float>(110 * j), static_cast<float>(110 * i)));
-			crosses[i][j][1].rotate(135);
+	int dimension;
+	int winCondition;
+	char playerTag;
+	char botTag;
+	bool changeSettings;
+	do {
+		std::cout << "Dim: ";
+		std::cin >> dimension;
+		while(std::cin.fail() || dimension < 3 && dimension > 10) {
+			std::cerr << "Wrong dim!" << std::endl;
+			std::cin.clear();
+			std::cin.ignore(10000, '\n');
+			std::cout << "Dim: ";
+			std::cin >> dimension;
 		}
-	}
-	while(window.isOpen()) {
-		scoreText = "Player " + std::to_string(playerScore) + ":" + std::to_string(botScore) + " Bot";
-		scoreBanner.setString(scoreText);
-		scoreBanner.setOrigin(scoreBanner.getLocalBounds().width / 2, scoreBanner.getLocalBounds().height / 2);
-		scoreBanner.setPosition(static_cast<float>(55 * dimension - 12), 10);
-		sf::Event event;
-		while(window.pollEvent(event)) {
-			if(event.type == sf::Event::Closed)
-				if(confirm('q')) {
-					window.close();
-				}
-			if(event.type == sf::Event::MouseButtonPressed) {
-				if(event.mouseButton.button == sf::Mouse::Left)
-					if(field(event, dimension, sq) != -1) {
-						int but = field(event, dimension, sq);
-						if(board(but / dimension, but % dimension) == 0) {
-							board(but / dimension, but % dimension) = playerTag;
-							if(board.checkWin(winCondition, 'x')) {
-								++playerScore;
-								if(gameOver(playerScore, botScore, 'w'))
-									board.clear();
-								else
-									window.close();
+		std::cout << "Win: ";
+		std::cin >> winCondition;
+		while(std::cin.fail() || winCondition > dimension) {
+			std::cerr << "Wrong win!" << std::endl;
+			std::cin.clear();
+			std::cin.ignore(10000, '\n');
+			std::cout << "Win: ";
+			std::cin >> winCondition;
+		}
+		std::cout << "Tag: ";
+		std::cin >> playerTag;
+		while(std::cin.fail() || playerTag != 'x' && playerTag != 'X' && playerTag != 'o' && playerTag != 'O') {
+			std::cerr << "Wrong tag!" << std::endl;
+			std::cin.clear();
+			std::cin.ignore(10000, '\n');
+			std::cout << "Tag: ";
+			std::cin >> playerTag;
+		}
+		char botTag = playerTag == 'x' ? 'o' : 'x';
+		int playerScore = 0;
+		int botScore = 0;
+		changeSettings = false;
+		Board board(dimension);
+		sf::Font font;
+		font.loadFromFile("arial.ttf");
+		sf::Text scoreBanner;
+		scoreBanner.setFont(font);
+		scoreBanner.setCharacterSize(25);
+		scoreBanner.setFillColor(sf::Color::Black);
+		std::string scoreText;
+		sf::ContextSettings windowSettings;
+		windowSettings.antialiasingLevel = 16;
+		sf::RenderWindow window(sf::VideoMode(10 + 110 * dimension, 110 * dimension + 40), "Tic-Tac-Toe", sf::Style::Titlebar | sf::Style::Close, windowSettings);
+		sf::RectangleShape upperBar(sf::Vector2f(static_cast<float>(10 + 110 * dimension), 30));
+		upperBar.setFillColor(sf::Color(200, 200, 200));
+		std::unique_ptr<std::unique_ptr<sf::RectangleShape[]>[]> sq = std::make_unique<std::unique_ptr<sf::RectangleShape[]>[]>(dimension);;
+		for(int i = 0; i < dimension; ++i) sq[i] = std::make_unique<sf::RectangleShape[]>(dimension);
+		std::unique_ptr<std::unique_ptr<sf::CircleShape[]>[]> circles = std::make_unique<std::unique_ptr<sf::CircleShape[]>[]>(dimension);
+		for(int i = 0; i < dimension; ++i) circles[i] = std::make_unique<sf::CircleShape[]>(dimension);
+		std::unique_ptr<std::unique_ptr<sf::RectangleShape[][2]>[]> crosses = std::make_unique<std::unique_ptr<sf::RectangleShape[][2]>[]>(dimension);
+		for(int i = 0; i < dimension; ++i) crosses[i] = std::make_unique<sf::RectangleShape[][2]>(dimension);
+		for(int i = 0; i < dimension; ++i) {
+			for(int j = 0; j < dimension; ++j) {
+				sq[i][j].setSize(sf::Vector2f(100, 100));
+				sq[i][j].setFillColor(sf::Color(200, 200, 200));
+				sq[i][j].setPosition(sf::Vector2f(10, 40));
+				sq[i][j].move(sf::Vector2f(static_cast<float>(110 * j), static_cast<float>(110 * i)));
+				circles[i][j].setRadius(40);
+				circles[i][j].setPointCount(20);
+				circles[i][j].setPosition(sf::Vector2f(20, 50));
+				circles[i][j].move(sf::Vector2f(static_cast<float>(110 * j), static_cast<float>(110 * i)));
+				circles[i][j].setFillColor(sf::Color(200, 200, 200));
+				circles[i][j].setOutlineThickness(-5);
+				circles[i][j].setOutlineColor(sf::Color::Black);
+				crosses[i][j][0].setSize(sf::Vector2f(113, 5));
+				crosses[i][j][0].setFillColor(sf::Color::Black);
+				crosses[i][j][0].setOrigin(57, 3);
+				crosses[i][j][0].setPosition(sf::Vector2f(60, 91));
+				crosses[i][j][0].move(sf::Vector2f(static_cast<float>(110 * j), static_cast<float>(110 * i)));
+				crosses[i][j][0].rotate(45);
+				crosses[i][j][1].setSize(sf::Vector2f(113, 5));
+				crosses[i][j][1].setFillColor(sf::Color::Black);
+				crosses[i][j][1].setOrigin(57, 3);
+				crosses[i][j][1].setPosition(sf::Vector2f(59, 90));
+				crosses[i][j][1].move(sf::Vector2f(static_cast<float>(110 * j), static_cast<float>(110 * i)));
+				crosses[i][j][1].rotate(135);
+			}
+		}
+		while(window.isOpen()) {
+			scoreText = "Player " + std::to_string(playerScore) + ":" + std::to_string(botScore) + " Bot";
+			scoreBanner.setString(scoreText);
+			scoreBanner.setOrigin(scoreBanner.getLocalBounds().width / 2, scoreBanner.getLocalBounds().height / 2);
+			scoreBanner.setPosition(static_cast<float>(55 * dimension - 12), 10);
+			sf::Event event;
+			while(window.pollEvent(event)) {
+				if(event.type == sf::Event::Closed)
+					if(confirm('q')) {
+						window.close();
+					}
+				if(event.type == sf::Event::MouseButtonPressed) {
+					if(event.mouseButton.button == sf::Mouse::Left) {
+						if(event.mouseButton.x >= upperBar.getPosition().x &&
+							 event.mouseButton.x <= upperBar.getPosition().x + upperBar.getSize().x &&
+							 event.mouseButton.y >= upperBar.getPosition().y &&
+							 event.mouseButton.y <= upperBar.getPosition().y + upperBar.getSize().y) {
+							if(confirm('c')) {
+								window.close();
+								changeSettings = true;
+							}
+						}
+						if(field(event, dimension, sq) != -1) {
+							int but = field(event, dimension, sq);
+							if(board(but / dimension, but % dimension) == 0) {
+								board(but / dimension, but % dimension) = playerTag;
+								if(board.checkWin(winCondition, 'x')) {
+									++playerScore;
+									if(gameOver(playerScore, botScore, 'w'))
+										board.clear();
+									else
+										window.close();
+								}
 							}
 						}
 					}
-			}
-		}
-		window.clear(sf::Color::White);
-		for(int i = 0; i < dimension; ++i) {
-			for(int j = 0; j < dimension; ++j) {
-				window.draw(sq[i][j]);
-				switch(board(i, j)) {
-					case 'x':
-						window.draw(crosses[i][j][0]);
-						window.draw(crosses[i][j][1]);
-						break;
-					case 'o':
-						window.draw(circles[i][j]);
-						break;
 				}
 			}
+			window.clear(sf::Color::White);
+			for(int i = 0; i < dimension; ++i) {
+				for(int j = 0; j < dimension; ++j) {
+					window.draw(sq[i][j]);
+					switch(board(i, j)) {
+						case 'x':
+							window.draw(crosses[i][j][0]);
+							window.draw(crosses[i][j][1]);
+							break;
+						case 'o':
+							window.draw(circles[i][j]);
+							break;
+					}
+				}
+			}
+			window.draw(upperBar);
+			window.draw(scoreBanner);
+			window.display();
 		}
-		window.draw(upperBar);
-		window.draw(scoreBanner);
-		window.display();
-	}
+	} while(changeSettings);
 	return 0;
 }
