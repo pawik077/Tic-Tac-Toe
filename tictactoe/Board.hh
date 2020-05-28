2abcc5395d7c00de077661b6c9ec1b241f9e1f15
pawik077 <pawik.r.s.077@gmail.com>
2020-05-28 16:48:03 +0200

    Moved checkColumns, checkRows and checkDiagonals methods to private

--- tictactoe/Board.hh
+++ tictactoe/Board.hh
@@ -6,15 +6,15 @@ class Board {
 private:
 	std::unique_ptr<std::unique_ptr<char[]>[]> _board;
 	int _dimension;
+	bool checkColumns(int winCond, char tag) const;
+	bool checkRows(int winCond, char tag) const;
+	bool checkDiagonals(int winCond, char tag) const;
 public:
 	Board(int dimension);
 	const char operator () (int x, int y) const { return _board[x][y]; }
 	char& operator () (int x, int y) { return _board[x][y]; }
 	const int getDimension() const { return _dimension; };
 	void clear();
-	bool checkColumns(int winCond, char tag) const;
-	bool checkRows(int winCond, char tag) const;
-	bool checkDiagonals(int winCond, char tag) const;
 	bool checkWin(int winCond, char tag) const;
 	bool checkFull() const;
 };
