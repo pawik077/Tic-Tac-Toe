95bdeaccfb317fc4633456f72170b07d258e90ee
pawik077 <pawik.r.s.077@gmail.com>
2020-05-31 18:53:31 +0200

    Added alpha-beta pruning to optimize minMax algorithm

--- tictactoe/AI.cpp
+++ tictactoe/AI.cpp
@@ -1,20 +1,23 @@
 #include "AI.hh"
 
-int AI::minMax(Board& board, int depth, bool isMax) {
+int AI::minMax(Board& board, int depth, int alpha, int beta,  bool isMax) {
 	char playerTag = _botTag == 'o' ? 'x' : 'o';
 	if(depth == 0) return 0;
 	if(board.checkWin(_botTag)) return 10 + depth;
 	if(board.checkWin(playerTag)) return -10 - depth;
-	if(board.checkFull()) return 0;
+	if(board.checkFull()) return 1;
 	if(isMax) {
 		int best = -1000000;
 		std::list<int> moves = board.getEmpty();
 		for(int move : moves) {
 			board(move / board.getDimension(), move % board.getDimension()) = _botTag;
 			std::cout << "bot calc";
-			int solution = minMax(board, depth - 1, !isMax);
-			best = best > solution ? best : solution;
+			int solution = minMax(board, depth - 1, alpha, beta, !isMax);
 			board(move / board.getDimension(), move % board.getDimension()) = 0;
+			best = best > solution ? best : solution;
+			alpha = alpha > solution ? alpha : solution;
+			if(beta <= alpha)
+				break;
 		}
 		return best;
 	} else {
@@ -23,9 +26,12 @@ int AI::minMax(Board& board, int depth, bool isMax) {
 		for(int move : moves) {
 			board(move / board.getDimension(), move % board.getDimension()) = playerTag;
 			std::cout << "player calc";
-			int solution = minMax(board, depth - 1, !isMax);
-			best = best < solution ? best : solution;
+			int solution = minMax(board, depth - 1, alpha, beta, !isMax);
 			board(move / board.getDimension(), move % board.getDimension()) = 0;
+			best = best < solution ? best : solution;
+			beta = beta < solution ? beta : solution;
+			if(beta <= alpha)
+				break;
 		}
 		return best;
 	}
@@ -38,7 +44,7 @@ void AI::move(Board& board) {
 	std::list<int> moves = board.getEmpty();
 	for(int move : moves) {
 		board(move / board.getDimension(), move % board.getDimension()) = _botTag;
-		int moveScore = minMax(board, depth, false);
+		int moveScore = minMax(board, depth, -1000000, 1000000, false);
 		board(move / board.getDimension(), move % board.getDimension()) = 0;
 		if(moveScore > bestScore) {
 			bestMove = move;
--- tictactoe/AI.hh
+++ tictactoe/AI.hh
@@ -4,7 +4,7 @@
 
 class AI {
 	char _botTag;
-	int minMax(Board& board, int depth, bool isMax);
+	int minMax(Board& board, int depth, int alpha, int beta, bool isMax);
 public:
 	AI(char botTag) { _botTag = botTag; }
 	const char getTag() { return _botTag; }
